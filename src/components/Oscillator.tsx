b63db499750959bca1ff477bfadacb59c6c06ae5
Robin Lake <87996036+RobinTempusEx@users.noreply.github.com>
2021-11-28 21:57:37 -0700

    accepts microphone input

--- src/components/Oscillator.tsx
+++ src/components/Oscillator.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+// import React, {useState, useEffect} from 'react';
 import NumberSlider from './NumberSlider';
 
 const Oscillator = () => {
 
+    // const [audioSource, setAudioSource] = useState();
+
+    // useEffect(() => {
+    //     const getAudioSource = async () => {
+    //         return navigator.mediaDevices.getUserMedia({
+    //             audio: {
+    //                 echoCancellation: false,
+    //                 autoGainControl: false,
+    //                 noiseSuppression: false,
+    //                 latency: 0
+    //             }
+    //         })
+    //     }
+    //     const mySource = await getAudioSource()
+    //     const source = audioSource.createMediaStreamSource(mySource);
+    //     setAudioSource(source);
+    //     source.connect(audioContext.destination);
+
+    // }, [audioSource]);
     const audioContext = new AudioContext();
+
+
+    const getAudioSource = () => {
+        navigator.mediaDevices.getUserMedia({
+            audio: {
+                echoCancellation: false,
+                autoGainControl: false,
+                noiseSuppression: false,
+                latency: 0
+            }
+        })
+        .then(stream => {
+            const source = audioContext.createMediaStreamSource(stream);
+            console.log("stream: ", stream);
+            source.connect(audioContext.destination);
+
+        })
+        .catch(error => console.error(error))
+    }
+    // const mySource = getAudioSource()
+    getAudioSource();
+    // const source = audioSource.createMediaStreamSource(mySource);
+    // setAudioSource(source);
+    // source.connect(audioContext.destination);
+
+
+
     let osc = audioContext.createOscillator();
     const gain = audioContext.createGain();
 
